0adc3b8b88e509b48d86febd77731d420d4113fb
Jia Song <jsong@6grain.com>
2019-04-12 17:25:27 +0800

    Merge branch 'master' of /Users/jsong/Documents/code/EOSprite with conflicts.

--- eos-data/src/test/java/cn/geosprite/eosdata/dao/DataGranuleRepositoryTest.java
+++ eos-data/src/test/java/cn/geosprite/eosdata/dao/DataGranuleRepositoryTest.java
@@ -1,6 +1,14 @@
 package cn.geosprite.eosdata.dao;
 
+import cn.geosprite.eosdata.entity.DataGranule;
+import cn.geosprite.eosdata.entity.OrderDataGranule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,9 +18,29 @@ import static org.junit.Assert.*;
  * @ Description：None
  * @ Modified By：
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DataGranuleRepositoryTest {
+    @Autowired
+    OrderDataGranuleRepository orderDataGranuleRepository;
 
     @Test
     public void findDataGranulesByDataGranuleId() {
+        List<OrderDataGranule> list = orderDataGranuleRepository.findOrderDataGranulesByOrderId(1);
+        list.forEach(x -> System.out.println(x.toString()));
+//
+//        OrderDataGranule d = new OrderDataGranule();
+//        DataGranule g = new DataGranule();
+//        g.setDataGranuleId("LC08/L1TP_C1_T1/117050/2019-09-21");
+//        g.setSensorCode("sensorCode");
+//        g.setProductCode("proCode");
+//        g.setTileCode("tileCode");
+//        g.setSceneDate(new java.sql.Date(System.currentTimeMillis()));
+//        g.setFormatCode("forCode");
+//        g.setDataSource("source");
+//        d.setOrderId(1);
+//        d.setDataGranule(g);
+//
+//        orderDataGranuleRepository.save(d);
     }
 }
