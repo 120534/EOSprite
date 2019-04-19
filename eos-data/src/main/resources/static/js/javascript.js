61ce6080e5728538e14fe8efe0a2b1207f124e60
wanghanlin <lhkwhl@qq.com>
2019-04-19 21:29:47 +0800

    数据存储路径修改

--- eos-data/src/main/resources/static/js/javascript.js
+++ eos-data/src/main/resources/static/js/javascript.js
@@ -7,8 +7,70 @@ $(function() {
 });
 
 $('.tab-nav li').click(function(){
-    $('.tab-nav li').removeClass('current');
-    $(this).addClass('current');
     $('.tab-content').css('display','none');
     $('.tab-content').eq($(this).index()).css('display','block')
 })
+
+var map = L.map('map',{zoomControl: false}).setView([35, 110], 4);
+L.tileLayer('https://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}', {
+    subdomains: ["1", "2", "3", "4"], //可用子域名，用于浏览器并发请求
+    attribution: "&copy; 高德地图", //可以修改为其它内容
+}).addTo(map); //添加tile层到地图
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
+
+//先只做了数字校验,post一共提交了七个字段。
+//提前处理一下
+$("#btn-1").click(function(){
+    const dataSet = $("#select-dataSet").val();
+    const productCode = $("#select-product").val();
+    const startPath = $("#startPath").val();
+    const endPath = $("#endPath").val();
+    const startRow = $("#startRow").val();
+    const endRow = $("#endRow").val();
+    const p = /^[0-9]*$/;
+
+    //判断是否选择数据集
+    if (dataSet == null){
+        alert("请选择数据集");
+        return false;
+    }
+    if (productCode == null){
+        alert("请选择数据产品");
+        return false;
+    }
+
+    if (startPath === "" || endPath === ""){
+        alert("path不能为空!");
+        return false;
+    }
+    
+    if (!p.test(startPath) || !p.test(endPath))
+    {
+        alert("path必须为数字!");
+        return false;
+    }
+    if (startRow === "" || endRow === ""){
+        alert("row不能为空!");
+        return false;
+    }
+    if (!p.test(startRow) || !p.test(endRow))
+    {
+        alert("Row必须为数字!");
+        return false;
+    }
+
+    $.post("/home",{
+        'productCode':val1,
+        'tileCode':val2,
+        'startRow':v
+    });
+
+});
+
+function doPost(url)
+{
+    var val1 = $("#startPath").val();
+    var val2 =$("#endPath").val();
+
+    $.post("/home",{'key1':val1, 'key2':val2});
+}
